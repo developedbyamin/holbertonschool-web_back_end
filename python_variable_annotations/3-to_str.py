eadb8868b3722194219838db18a6cb88236512b2
developedbyamin <developedbyamin@gmail.com>
2024-04-13 18:55:09 +0400

    first commit

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''
+Description: function floor which
+takes a float n as argument and returns the floor of the float.
+Arguments: n: float
+'''
+
+def to_str(n: float) -> str:
+    ''' Return str of n. '''
+    return str(n) 
