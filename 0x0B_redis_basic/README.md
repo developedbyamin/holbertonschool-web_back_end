72963985a06970290fac3464d480ef17728808d0
developedbyamin <developedbyamin@gmail.com>
2024-05-15 18:31:01 +0400

    redis

---
+++ 0x0B_redis_basic/README.md
@@ -0,0 +1,8 @@
+# Redis Cache
+
+## Overview
+This project demonstrates the usage of Redis as a caching mechanism in Python applications.
+
+## Requirements
+- Python 3.7
+- Redis server
--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,12 +1,35 @@
+#!/usr/bin/env python3
+
+"""
+Cache module to store data in Redis.
+"""
+
 import redis
 import uuid
 from typing import Union
 
 class Cache:
-    def __init__(self):
+    """
+    Cache class to store data in Redis.
+    """
+    def __init__(self) -> None:
+        """
+        Initialize the Cache instance with a Redis client.
+        """
         self._redis = redis.Redis()
         self._redis.flushdb()
-    def store(self,data: Union[str, bytes, int, float]):
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """
+        Store data in Redis using a randomly generated key.
+
+        Args:
+            data: The data to be stored. Can be str, bytes, int, or float.
+
+        Returns:
+            str: The randomly generated key used for storing the data in Redis.
+        """
         key = str(uuid.uuid4())
-        self._redis.set(key,data)
+        self._redis.set(key, data)
         return key
+
