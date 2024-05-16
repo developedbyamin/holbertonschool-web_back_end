84f13c2a9af391e3622897d15cb1594e60ba9de7
developedbyamin <developedbyamin@gmail.com>
2024-05-16 11:02:53 +0400

    redis1

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -33,19 +33,16 @@ class Cache:
         self._redis.set(key, data)
         return key
 
-    """
-    Cache class to store data in Redis.
-    """
-    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float]:
-        if not self._redis.exists(key):
-            return None:
-        value = self._redis.get(key)
+    def get(self, key: str, fn: Optional[Callable] = None) -> Union[str, bytes, int, None]:
+        data = self._redis.get(key)
+        if data is None:
+            return None
         if fn:
-            return fn(value)
-        return value
+            return fn(data)
+        return data
 
-        def get_str(self,key: str) -> Union[str, None]:
-            return self.get(key, fn=lambda d: d.dcode("utf-8"))
-    
-        def get_int(self, key: str) -> Union[int, None]:
-        return self.get(key, fn=int)                
+    def get_str(self, string: bytes) -> str:
+        return string.decode("utf-8")
+
+    def get_int(self, number: int) -> int:
+        return int(number)
