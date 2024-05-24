8efe8e57985d523bff24ccb07d3a05ced0f6dd3a
developedbyamin <developedbyamin@gmail.com>
2024-05-24 14:51:06 +0400

    NoSQL

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 """ List all documents in Python  """
 from pymongo import MongoClient
-def list_all(school_collection):
-    findcollection = school_collection.find()
-    return findcollection
+
+
+cliend = MongoClient('localhost', 27017)
+
+def list_all(mongo_collection):
+    ''' List all documents
+    '''
+    documents = client.mongo_collection.find()
+    return documents
