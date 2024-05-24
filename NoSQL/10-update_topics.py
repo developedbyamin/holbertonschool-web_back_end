c0ee0f712a8268a2192b3181589b5b9e12c6b215
developedbyamin <developedbyamin@gmail.com>
2024-05-24 16:01:34 +0400

    NoSQL

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+    Python function that changes all topics of a school document based on the name
+"""   
+
+from pymongo import MongoClient
+
+def update_topics(mongo_collection, name, topics):
+    """
+    Change all topics of a school document based on the name.
+
+    :param mongo_collection: The pymongo collection object
+    :param name: The school name to update
+    :param topics: The list of topics approached in the school
+    """
+    mongo_collection.update_many(
+        {"name": "Holberton School"},
+        {"$set": {"topics": topics}},
+    )
