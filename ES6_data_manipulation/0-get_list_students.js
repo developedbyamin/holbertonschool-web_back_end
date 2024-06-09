5ae5001e0b4dc46e34e54f8a8d80d71a5cffd5fe
developedbyamin <developedbyamin@gmail.com>
2024-06-09 03:26:47 -0400

    getListStudent

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,13 +1,21 @@
 // Get List Students
 
-const getListStudents = () => {
-    const students = [
-      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-      { id: 2, firstName: 'James', location: 'Columbia' },
-      { id: 5, firstName: 'Serena', location: 'San Francisco' },
-    ];
-  
+class Student{
+    constructor(id, firstName, location){
+        this.id = id;
+        this.firstName = firstName;
+        this.location = location;
+    }
+}
+
+const students = [
+    new Student(1, 'Guillaume', 'San Francisco'),
+    new Student(2, 'James', 'Columbia'),
+    new Student(5, 'Serena', 'San Francisco'),
+];  
+
+function getListStudents(){
     return students;
-  };
-  
-  export default getListStudents;
+}
+
+export default getListStudents;
