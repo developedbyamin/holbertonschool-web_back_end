4b3f4591368cce23b1f55b2eb68f12af2cbdd9ae
developedbyamin <developedbyamin@gmail.com>
2024-06-15 12:51:44 -0400

    node js basic

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,32 +1,32 @@
-function countStudents(filePath1){
-    const fs = require('fs');
-    const path = require('path');
-    const filePath = filePath1;
-    const absolutePath = path.resolve(filePath);
-    
-    fs.readFile(absolutePath, 'utf8', (err, data) => {
-        if(err){
-            throw new Error('Cannot load the database');
-        }
-        const rows = data.split('\n').map(row => row.split(','));
-        let totalStudents = 0;
-        let csStudents = 0;
-        let csStudentsNames = [];
-        let sweStudentsNames = [];
-        for(let i = 1; i < rows.length; i++){
-            totalStudents++;
-            if(rows[i][3] == 'CS'){
-                csStudents++;
-                csStudentsNames.push(rows[i][0]);
-            } else{
-                sweStudentsNames.push(rows[i][0]);
-            }
-        }
-        csStudentsNames1 = csStudentsNames.join(' ');
-        sweStudentsNames1 = sweStudentsNames.join(' ');
-        process.stdout.write(`Number of students: ${totalStudents}\nNumber of students in CS: 6. List: ${csStudentsNames}\nNumber of students in SWE: ${totalStudents-csStudents} List: ${sweStudentsNames}\n`);
-    });
+const fs = require('fs');
+const path = require('path');
 
+function countStudents(filePath1) {
+  const filePath = filePath1;
+  const absolutePath = path.resolve(filePath);
+
+  fs.readFile(absolutePath, 'utf8', (err, data) => {
+    if (err) {
+      throw new Error('Cannot load the database');
+    }
+    const rows = data.split('\n').map((row) => row.split(','));
+    let totalStudents = 0;
+    let csStudents = 0;
+    const csStudentsNames = [];
+    const sweStudentsNames = [];
+    for (let i = 1; i < rows.length; i += 1) {
+      totalStudents += 1;
+      if (rows[i][3] === 'CS') {
+        csStudents += 1;
+        csStudentsNames.push(rows[i][0]);
+      } else {
+        sweStudentsNames.push(rows[i][0]);
+      }
+    }
+    const csStudentsNamesString = csStudentsNames.join(' ');
+    const sweStudentsNamesString = sweStudentsNames.join(' ');
+    process.stdout.write(`Number of students: ${totalStudents}\nNumber of students in CS: ${csStudents}. List: ${csStudentsNamesString}\nNumber of students in SWE: ${totalStudents - csStudents}. List: ${sweStudentsNamesString}\n`);
+  });
 }
 
 module.exports = countStudents;
