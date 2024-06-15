9062a65b29ad62b4465cb9a949863e3d41a8d117
developedbyamin <developedbyamin@gmail.com>
2024-06-15 09:00:18 -0400

    node js basic

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -5,3 +5,6 @@ process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${name}\n`);
   process.exit();
 });
+process.on('exit', () => {
+  console.log('This important software is now closing');
+});
