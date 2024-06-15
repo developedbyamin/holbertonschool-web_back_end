1edd7503ebbc48208003492d983145609c826280
developedbyamin <developedbyamin@gmail.com>
2024-06-15 10:15:26 -0400

    node js basic

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,15 +1,8 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// Check if stdin is connected to a terminal (CLI)
-if (process.stdin.isTTY) {
-  // Display closing message only when interacting via CLI
-  process.on('beforeExit', () => {
-    console.log('This important software is now closing');
-  });
-}
-
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
   console.log(`Your name is: ${name}`);
+  console.log('This important software is now closing');
   process.exit();
 });
