7ea248d8ead8aa442adb631275f16a68ad2008c4
developedbyamin <developedbyamin@gmail.com>
2024-06-15 09:35:20 -0400

    node js basic

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,10 +1,8 @@
-// 1-stdin.js
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('data', (data) => {
   const name = data.toString().trim();
-  process.stdout.write(`Your name is: ${name}\n`);
-  process.exit();
-});
-process.on('exit', () => {
+  console.log(`Your name is: ${name}`);
   console.log('This important software is now closing');
+  process.exit();
 });
