958bf738d82de32470de37b890804a711802cd85
developedbyamin <developedbyamin@gmail.com>
2024-06-15 12:58:07 -0400

    node js basic

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -1,32 +1,43 @@
 const fs = require('fs');
 const path = require('path');
 
-function countStudents(filePath1) {
-  const filePath = filePath1;
+function countStudents(filePath) {
   const absolutePath = path.resolve(filePath);
 
-  fs.readFile(absolutePath, 'utf8', (err, data) => {
-    if (err) {
-      throw new Error('Cannot load the database');
+  try {
+    const data = fs.readFileSync(absolutePath, 'utf8');
+    const rows = data.split('\n').map((row) => row.split(',')).filter((row) => row.length > 1); // Filter out empty lines
+
+    if (rows.length <= 1) {
+      throw new Error('No valid student data found in the database');
     }
-    const rows = data.split('\n').map((row) => row.split(','));
+
     let totalStudents = 0;
     let csStudents = 0;
     const csStudentsNames = [];
     const sweStudentsNames = [];
+
     for (let i = 1; i < rows.length; i += 1) {
-      totalStudents += 1;
-      if (rows[i][3] === 'CS') {
-        csStudents += 1;
-        csStudentsNames.push(rows[i][0]);
-      } else {
-        sweStudentsNames.push(rows[i][0]);
+      if (rows[i].length >= 4 && rows[i][3]) {
+        totalStudents += 1;
+        if (rows[i][3] === 'CS') {
+          csStudents += 1;
+          csStudentsNames.push(rows[i][0]);
+        } else if (rows[i][3] === 'SWE') {
+          sweStudentsNames.push(rows[i][0]);
+        }
       }
     }
+
     const csStudentsNamesString = csStudentsNames.join(', ');
     const sweStudentsNamesString = sweStudentsNames.join(', ');
-    process.stdout.write(`Number of students: ${totalStudents}\nNumber of students in CS: ${csStudents}. List: ${csStudentsNamesString}\nNumber of students in SWE: ${totalStudents - csStudents}. List: ${sweStudentsNamesString}\n`);
-  });
+
+    console.log(`Number of students: ${totalStudents}`);
+    console.log(`Number of students in CS: ${csStudents}. List: ${csStudentsNamesString}`);
+    console.log(`Number of students in SWE: ${totalStudents - csStudents}. List: ${sweStudentsNamesString}`);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
 }
 
 module.exports = countStudents;
