94c70a4749250673d2ae9d139445a1284a9703f0
developedbyamin <developedbyamin@gmail.com>
2024-06-15 09:44:13 -0400

    node js basic

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,9 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+// 1-stdin.js
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdin.on('data', (data) => {
+  const name = data.toString().trim();
+  process.stdout.write(`Your name is: ${name}\n`);
 });
-
-rl.question('Welcome to Holberton School, what is your name?\n', (input) => {
-  console.log(`Your name is: ${input}`);
-  rl.close();
+process.on('exit', () => {
+  console.log('This important software is now closing');
 });
